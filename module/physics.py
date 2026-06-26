804c05bf07b05ef9e327e7ea0f5c60857065fa6f
superded <super-ded.42@yandex.ru>
2026-06-26 16:16:53 +0500

    Первые шаги

---
+++ module/physics.py
@@ -0,0 +1,27 @@
+import math
+
+#Блок 1 - без учета сопротивления воздуха и трения
+#===============================================
+# Ускорение
+def acceleration_noresist(k, m, x):
+    ax=-k/m*x   
+    return(ax)
+
+# Частота собственных колебаний
+def frequency_noresist(k, m):
+    omega=math.sqrt(k/m)
+    return(omega)
+
+# Период
+def period_noresist(k,m):
+    T=2*math.pi*math.sqrt(m/k)
+    return(T)
+
+# Закон синуса и косинуса:
+def sin_noresist(xm, t, omega, phi):
+    x=math.sin(omega*t+phi)
+    return(x)
+def cos_noresist(xm, t, omega, phi):
+    x=math.cos(omega*t+phi)
+    return(x)
+
